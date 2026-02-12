6ee919c29643995f6652326116b16670bd8d7226
plutowootoe <adamgreenan30@gmail.com>
2026-02-12 00:31:21 +0000

    Added Torrent Constructor

--- MyTorrentClient/Torrent.cs
+++ MyTorrentClient/Torrent.cs
@@ -26,23 +26,119 @@ namespace BitTorrent
 
         public int BlockSize { get; private set; }
         public int PieceSize { get; private set; }
-        public long TotalSize { get { return Files.Sum(x => x.Size);  } }
+        public long TotalSize { get { return Files.Sum(x => x.Size); } }
 
-        public string FormattedPieceSize { get { return BytesToString(PieceSize);  } }
-        public string FormattedTotalSize { get { return BytesToString(TotalSize);  } }
+        public string FormattedPieceSize { get { return BytesToString(PieceSize); } }
+        public string FormattedTotalSize { get { return BytesToString(TotalSize); } }
 
-        public int PieceCount { get; { return PieceHashes.Length;  } }
+        public int PieceCount { get { return PieceHashes.Length; } }
 
         public byte[][] PieceHashes { get; private set; }
         public bool[] IsPieceVerified { get; private set; }
         public bool[][] IsBlockAcquired { get; private set; }
 
-        
+        public string VerifiedPiecesString { get { return String.Join("", IsPieceVerified.Select(x => x ? 1 : 0)); } }
+        public int VerifiedPieceCount { get { return IsPieceVerified.Count(x => x); } }
+        public double VerifiedRatio { get { return VerifiedPieceCount / (double)PieceCount; } }
+        public bool IsCompleted { get { return VerifiedPieceCount == PieceCount; } }
+        public bool IsStarted { get { return VerifiedPieceCount > 0; } }
+
+        public long Uploaded { get; set; } = 0;
+        public long Downloaded { get { return PieceSize * VerifiedPieceCount; } }
+        public long Left { get { return TotalSize - Downloaded; } }
+
 
         public byte[] InfoHash { get; private set; } = new byte[20];
         public string HexStringInfoHash { get { return String.Join("", this.InfoHash.Select(x => x.ToString("x2"))); } }
         public string UrlSafeStringInfoHash { get { return Encoding.UTF8.GetString(WedUtility.UrlEncodeToBytes(this.InfoHash, 0, 20)); } }
-        
+
+
+        public int GetPieceSize(int piece)
+        {
+            if (piece == PieceCount - 1)
+            {
+                int remainder = Convert.ToInt32(TotalSize % PieceSize);
+                if (remainder != 0)
+                    return remainder;
+            }
+
+            return PieceSize;
+        }
+
+        public int GetBlockSize(int piece, int block)
+        {
+            if (block == GetBlockCount(piece) - 1)
+            {
+                int remainder = Convert.ToInt32(GetPieceSize(piece) % BlockSize);
+                if (remainder != 0)
+                    return remainder;
+            }
+
+            return BlockSize;
+        }
+
+        public int GetBlockCount(int piece)
+        {
+            return Convert.ToInt32(Math.Ceiling(GetPieceSize(piece) / (double)BlockSize));
+        }
+
+        public event EventHandler<List<IPEndPoint>> PeerListUpdated;
+
+        private object[] fileWriteLocks;
+        private static SHA1 sha1 = SHA1.Create();
+
+        public Torrent(string name, string location, List<FileItem> files, List<string> trackers, int pieceSize, byte[] pieceHashes = null, int blockSize = 16384, bool? isPrivate = false)
+        {
+            Name = name;
+            DownloadDirectory = location;
+            Files = files;
+            fileWriteLocks = new object[Files.Count];
+            for (int i = 0; i < this.Files.Count; i++)
+                fileWriteLocks[i] = new object();
+
+            if (trackers != null)
+            {
+                foreach (string url in trackers)
+                {
+                    Tracker tracker = new Tracker(url);
+                    Trackers.Add(tracker);
+                    tracker.PeerListUpdated = +HandlePeerListUpdated;
+                }
+            }
+
+            PieceSize = pieceSize;
+            BlockSize = blockSize;
+            IsPrivate = isPrivate;
+
+            int count = Convert.ToInt32(Math.Ceiling(TotalSize / Convert.ToDouble(PieceSize)));
+
+            PieceHashes = new byte[count][];
+            IsPieceVerified = new bool[count];
+            IsBlockAcquired = new bool[count];
+
+            for (int i = 0; i < PieceCount; i++)
+                IsBlockAcquired[i] = new bool[GetBlockCount(i)];
+
+
+            if (pieceHashes == null)
+            {
+                for (int i = 0; i < PieceCount; i++)
+                {
+                    PieceHashes[i] = new byte[20];
+                    Buffer.BlockCopy(pieceHashes, i * 20, PieceHashes[i], 0, 20);
+                }
+            }
+
+            object info = TorrentInfoToBEncodingObject(this);
+            byte[] bytes = BEncoding.Encode(info);
+            InfoHash = SHA1.Create().ComputeHash(bytes);
+
+            for (int i = 0; i < PieceCount; i++)
+                Verify(i);
+        }
+
+
+
 
     }
 }
